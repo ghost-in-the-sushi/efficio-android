8b02929bea775c9e5abfc1ac8e507e3ca06c800a
Redwarp <redwarp@gmail.com>
2019-05-12 12:27:16 +0200

    Create a LoadinButton
    A loading button extends from MaterialButton, but can be toggled, and display a loading spinner. Follows material design recommendation from here: https://material.io/design/components/progress-indicators.html#circular-progress-indicators

--- app/src/main/java/org/ghostinthesuhi/android/efficio/main/StoreFragment.kt
+++ app/src/main/java/org/ghostinthesuhi/android/efficio/main/StoreFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_store.*
 import org.ghostinthesuhi.android.efficio.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,17 +23,6 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class StoreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,23 +33,15 @@ class StoreFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_store, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StoreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StoreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadingButton.setOnClickListener {
+            loadingButton.isLoading = !loadingButton.isLoading
+        }
+
+        enableButton.setOnClickListener {
+            loadingButton.isEnabled = !loadingButton.isEnabled
+        }
     }
 }
--- app/src/main/java/org/ghostinthesuhi/android/efficio/tools/ResourcesUtils.kt
+++ app/src/main/java/org/ghostinthesuhi/android/efficio/tools/ResourcesUtils.kt
@@ -1,6 +1,10 @@
 package org.ghostinthesuhi.android.efficio.tools
 
 import android.content.Context
+import android.graphics.Color
+import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 
 fun Context.getStringByResourceName(name: String): String? {
     val identifier = resources.getIdentifier(name, "string", packageName)
@@ -8,5 +12,16 @@ fun Context.getStringByResourceName(name: String): String? {
         null
     } else {
         getString(identifier)
+    }
+}
+
+@ColorInt
+fun Context.getThemeColor(@AttrRes attributeName: Int): Int {
+    val typedValue = TypedValue()
+    val typedArray = obtainStyledAttributes(typedValue.data, intArrayOf(attributeName))
+    try {
+        return typedArray.getColor(0, Color.BLACK)
+    } finally {
+        typedArray.recycle()
     }
 }
